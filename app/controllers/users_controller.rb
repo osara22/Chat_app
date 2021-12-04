2910fafb6d3a24ec3d99893fa17b46a08c184986
osara22 <osaraiya22@gmail.com>
2021-12-04 19:16:33 +0000

    [Fix]インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,12 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to root_path
-      else
-        render :new
-      end
-
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   private
